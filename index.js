f9a720cdb80b7ca1b26cc61190ed760f195b6673
LingkonDash <cocrupon@gamil.com>
2026-05-19 00:35:10 +0600

    connected to mongodb and created appointments api route

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI
 
 app.use(cors());
 app.use(express.json());
+
+
+// Mongo client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Mongo run() function
+async function run() {
+  try {
+    
+    await client.connect(); 
+
+    const db = client.db('doc-appoint-a9');
+    const appointmentCollection =  db.collection('appointments');
+
+    app.get('/appointments', async (req, res) => {
+      const result = await appointmentCollection.find().toArray();
+      res.json(result)
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run();
+
 
 app.get('/', (req, res) => {
   res.send('server is running fine!')
